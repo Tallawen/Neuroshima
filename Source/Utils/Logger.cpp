bb0aa577ba31b5cfd96a64c75badbf643f05e7c6
Szymon Kuchnia <kszymon0@gmail.com>
2013-11-07 19:52:04 +0100

    Klasa loggera (brakuje jedynie przekazywania daty)

---
+++ Source/Utils/Logger.cpp
@@ -0,0 +1,101 @@
+#include "Logger.h"
+
+#include <cassert>
+
+#define ADD_ENUM_TO_MAP(map, name, txt) map[name] = txt
+
+#define ENUM_STR(map, name) map[name]
+
+namespace Utils {
+
+    /************************************************/
+    Logger::Logger(const Stream &outType, std::string filename, const Openmode &mode) {
+        ADD_ENUM_TO_MAP(messageMap, Message::Info,     "Info");
+        ADD_ENUM_TO_MAP(messageMap, Message::Warning,  "Warning");
+        ADD_ENUM_TO_MAP(messageMap, Message::Error,    "Error");
+        ADD_ENUM_TO_MAP(messageMap, Message::Critical, "Critical");
+
+        streamMode = outType;
+
+        if(outType == Stream::Consol)
+            stream = &std::cout;
+
+        else if(outType == Stream::File) {
+            if(filename.empty()) {
+                std::cerr << "Logger: Nieprawidlowa nazwa pliku" << std::endl;
+              return;
+            }
+
+            std::ios_base::openmode openmode = std::ios::out;
+
+            switch(mode) {
+                case Openmode::Append:   openmode |= std::ios::app;   break;
+                case Openmode::Truncate: openmode |= std::ios::trunc; break;
+                default: assert(false); break;
+            }
+
+            stream = new std::fstream(filename, openmode);
+
+            if(!dynamic_cast<std::fstream*>(stream)->is_open()) {
+                std::cerr << "Logger: Nie można otworzyć pliku: " << filename << std::endl;
+              return;
+            }
+        }
+
+        welcomeMsg();
+    }
+
+
+    /************************************************/
+    Logger::~Logger() {
+        if(streamMode == Stream::File) {
+            std::fstream *fstream = dynamic_cast<std::fstream*>(stream);
+
+            fstream->close();
+            delete fstream;
+        }
+    }
+
+
+    /************************************************/
+    Logger& Logger::operator<< (std::ostream& (*funPtr)(std::ostream&)) {
+        funPtr(*stream);
+      return *this;
+    }
+
+
+    /************************************************/
+    Logger& Logger::operator<< (const Message &val) {
+      return *this << ENUM_STR(messageMap, val);
+    }
+
+
+    /************************************************/
+    void Logger::enable(bool value) {
+        static std::streambuf *buff = nullptr; /* Zmienna przechowująca buffor na czas wyłączenia */
+
+        if(buff == nullptr && value == false) {
+            buff = stream->rdbuf();
+            stream->rdbuf(nullptr);
+
+        } else if(buff != nullptr && value == true) {
+            stream->rdbuf(buff);
+            buff = nullptr;
+        }
+    }
+
+
+    /************************************************/
+    bool Logger::isEnable() const {
+      return stream->rdbuf() != nullptr;
+    }
+
+
+    /************************************************/
+    void Logger::welcomeMsg() {
+        *stream << std::endl
+                << "--- Log utworzony " << 0 << std::endl //TODO: Wpiąć metodę gdy będzie gotowa
+                << std::endl << std::endl;
+    }
+
+}
---
+++ Source/Utils/Logger.h
@@ -0,0 +1,176 @@
+#ifndef __UTILS_LOGGER_H__
+#define __UTILS_LOGGER_H__
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <map>
+
+#ifndef LOG_INFO
+#define LOG_INFO(lmsg) "DATE" << "| " << __FILE__ << "| " << __LINE__ << "| " << lmsg << "| " << __FUNCTION__ << ": "
+#endif /*LOG_INFO*/
+
+namespace Utils {
+
+    /**********************************************//**
+      @brief Logger
+     ************************************************/
+    class Logger {
+    public:
+        /**********************************************//**
+          Typ wiadomości
+         ************************************************/
+        enum class Message {
+            Info,
+            Warning,
+            Error,
+            Critical
+        };
+
+
+        /**********************************************//**
+          Rodzaj wyjścia na jaki mają zastać wypisywane wiadomości
+         ************************************************/
+        enum class Stream {
+            File,
+            Consol
+        };
+
+
+        /**********************************************//**
+          Tryb otwarcia pliku. Działa tylko z @b Stream::File
+         ************************************************/
+        enum class Openmode {
+            Append,
+            Truncate
+        };
+
+    public:
+        std::ostream *stream; /**< Wyjście na które zostaną przekazane dane */
+
+    private:
+        std::map<Message, const char*> messageMap; /**< Mapa do parsowania enum @see Message na string */
+
+        Stream streamMode; /**< Tryb działania parsera (czy zapisuje do pliku czy na konsole) */
+
+    public:
+        /**********************************************//**
+          Konstruktor
+
+          @param out      Określa gdzie mają być wypisywanie komunikaty
+          @param filename Nazwa pliku do którego mają zostać zapisywane komunikaty przy wybirze wyjścia @b Stream::File
+          @param mode     Określa sposób otwarcia pliku. Działą tylko z trybem @b Stream::File
+         ************************************************/
+        Logger(const Stream &out = Stream::Consol, std::string filename = "", const Openmode &mode = Openmode::Truncate);
+
+
+        /************************************************/
+        ~Logger();
+
+    public:
+        /**********************************************//**
+          @brief Operator wypisująca na wyjście @b stream
+
+          Operator obsługuje manupulatory które przyjmują i zwracają @b std::ostream&
+
+          @param funPtr Adres do funkcji która ma zostać obsłużona
+
+          @return Referencję na obiekt logger'a
+         ************************************************/
+        Logger& operator<< (std::ostream& (*funPtr)(std::ostream&));
+
+
+        /**********************************************//**
+          @brief Operator wypisujący na wyjście @b stream
+
+          @param val Wartość do wypisania
+
+          @return Referencję na obiekt logger'a
+         ************************************************/
+        Logger& operator<< (const int &val) { return write(val); };
+
+
+        /**********************************************//**
+          @copydoc operator<<(const int &val)
+         ************************************************/
+        Logger& operator<< (const float &val) { return write(val); };
+
+
+        /**********************************************//**
+          @copydoc operator<<(const int &val)
+         ************************************************/
+        Logger& operator<< (const double &val) { return write(val); };
+
+
+        /**********************************************//**
+          @copydoc operator<<(const int &val)
+         ************************************************/
+        Logger& operator<< (const char *val) { return write(val); };
+
+
+        /**********************************************//**
+          @copydoc operator<<(const int &val)
+         ************************************************/
+        Logger& operator<< (const std::string &val) { return write(val); };
+
+
+        /**********************************************//**
+          @copydoc operator<<(const int &val)
+         ************************************************/
+        Logger& operator<< (const Message &val);
+
+
+        /**********************************************//**
+          @brief Wyłącza/włącza zapis do logu
+         ************************************************/
+        void enable(bool value = true);
+
+
+        /**********************************************//**
+          @brief Czy logger jest włączony
+         ************************************************/
+        bool isEnable() const;
+
+    private:
+        /**********************************************//**
+          @brief Zwraca wiadomość powitalną (przy inicjalizowaniu loggera)
+         ************************************************/
+        inline void welcomeMsg();
+
+
+        /**********************************************//**
+          Metoda potrzebna do przeciążenia operatorów.
+          Teoretycznie można pominąć, ale wystąpi kopiowanie kodu w dużej ilości miejsc.
+
+          @note Ech ta kontrola typów, lubię ją, ale czasami daję popalić.
+         ************************************************/
+        template<typename T> Logger& write(const T &val) {
+            *stream << val;
+          return *this;
+        }
+
+    };
+
+}
+
+typedef Utils::Logger::Message LMsg; /**< Skrócony dostęp do @link Utils::Logger::Message */
+
+/**********************************************//**
+   @class Utils::Logger Logger.h "Utils/Logger.h"
+   @ingroup Utils
+
+   Klasa logger'a.
+   Klasa nie jest singletonem ponieważ może wystąpić parę loggerów.
+
+   @example
+      @code
+      Utils::Logger log(Utils::Logger::File, "log.txt");
+
+      log << LOG_INFO(LMsg::Info) << "Test ;)" << std::endl;
+      @endcode
+
+   @note Zalecam zawsze używać makra LOG_INFO i utworzyć obiekt globalnie.
+ ************************************************/
+
+#endif /*__UTILS_LOGGER_H__*/

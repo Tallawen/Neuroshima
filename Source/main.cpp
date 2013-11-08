f6daa20d15d9c0d01ffb334e0736051d8cf95d34
Szymon Kuchnia <kszymon0@gmail.com>
2013-11-08 14:08:12 +0100

    Dodanie pętli czasu rzeczywistego

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,21 @@
 #include "StdAfx.h"
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 16;
+
+    Renderer::Window window(sf::VideoMode(800, 600), "Window", sf::Style::Default, settings);
+
+     sf::Event event;
+    while(window.isOpen()) {
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
   return 0;
 }
